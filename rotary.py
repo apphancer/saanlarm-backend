70b91b73b003ecfa8ddfc3423fbf802816f147ba
Martin <martin@apphancer.com>
2024-11-17 21:57:14 +0100

    Fix typo in sw_callback definition and correct callback usage
    Fixed a missing colon in the sw_callback function definition. Also corrected the chg_callback parameter to use the correct change_callback function in my_encoder.setup().

--- rotary.py
+++ rotary.py
@@ -5,13 +5,13 @@ from config import ROTARY_CLK, ROTARY_DT, ROTARY_SW
 def change_callback(scale_position):
     print('Scale position is {}'.format(scale_position))
 
-def sw_callback(something)
+def sw_callback(something):
     print('Received switch callback with:', repr(something))
 
 my_encoder = pyky040.Encoder(CLK=ROTARY_CLK, DT=ROTARY_DT, SW=ROTARY_SW)
 
 def start_rotary():
-    my_encoder.setup(scale_min=0, scale_max=20, step=1, loop=True, chg_callback=my_callback, sw_callback=sw_callback)
+    my_encoder.setup(scale_min=0, scale_max=20, step=1, loop=True, chg_callback=change_callback, sw_callback=sw_callback)
     my_encoder.watch()
 
 def start_rotary_thread():
